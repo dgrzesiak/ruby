9608393c8522a7540cbc74504483aa676b94316b
dgrzesiak <grzesiak92@gmail.com>
2014-10-23 15:26:10 +0200

    Update klasy.rb

--- klasy.rb
+++ klasy.rb
@@ -6,10 +6,9 @@ end
 
 #do tej klasy trzeba będzie dodać jakiś inicjalizator
 class Kot
-	def initialize
-		print "Kot zostal dodany\n"
-	end
-	
+  def initialize
+    print "Kot zostal dodany\n"
+  end
 end
 #teraz warto dodać do tego wywołanie
 $kot = Kot.new
@@ -17,12 +16,12 @@ $kot = Kot.new
 #i ta klasa zostanie wywołana w momencie deklaracji Kot.new
 #warto by było dodać do tego nową metodę np koty lubią mleko
 class Kot
-def initialize
-		print "Kot zostal dodany\n"
-	end
-def mleko
-		print "Kot dostal mleko\n"
-	end
+  def initialize
+    print "Kot zostal dodany\n"
+  end
+  def mleko
+    print "Kot dostal mleko\n"
+  end
 end
 #aby to wywołać wystarczy dodać
 
@@ -32,18 +31,17 @@ end
 #a teraz załóżmy ze koty maja 7 żyć
 $zycie= 7
 class Kot
-def initialize
-		print "Kot zostal dodany\n"
-	end
-def mleko
-		print "Kot dostal mleko\n"
-	end
-def zycie
-		
-		print ("ten kot ma :")
-		print $zycie
-		print (" zyc\n")
-	end
+  def initialize
+    print "Kot zostal dodany\n"
+  end
+  def mleko
+    print "Kot dostal mleko\n"
+  end
+  def zycie
+    print ("ten kot ma :")
+    print $zycie
+    print (" zyc\n")
+  end
 end
 	
 #$kot.zycie
@@ -52,23 +50,21 @@ end
 #wiemy ze koty spadaja na 4 łapy i załóżmy ze traca przez to 1 życie
 class Kot
 $zycie= 7
-def initialize
-		print "Kot zostal dodany\n"
-	end
-def mleko
-		print "Kot dostal mleko\n"
-	end
-def zycie
-		
-		print "ten kot ma: ",$zycie," zyc\n"
-	end
-def utrata(y)
-		if y== 1
-		$zycie = $zycie - 1
-		print"niestety twoj kot spadl na cztery lapy\n","kot ma teraz: ",$zycie,"\n"
-		end
-		
-	end
+  def initialize
+    print "Kot zostal dodany\n"
+  end
+  def mleko
+    print "Kot dostal mleko\n"
+  end
+  def zycie
+    print "ten kot ma: ",$zycie," zyc\n"
+  end
+  def utrata(y)
+    if y== 1
+    $zycie = $zycie - 1
+    print"niestety twoj kot spadl na cztery lapy\n","kot ma teraz: ",$zycie,"\n"
+    end
+  end
 end
 
 #$kot.utrata(1)
